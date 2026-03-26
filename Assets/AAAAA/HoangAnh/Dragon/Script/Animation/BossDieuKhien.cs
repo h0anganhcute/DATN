8c7b83bf7e90e819e790a64ef39c21ae25483e5d
h0anganhcute <h0anganhcute66@gmail.com>
2026-03-27 00:59:17 +0700

    ok

---
+++ Assets/AAAAA/HoangAnh/Dragon/Script/Animation/BossDieuKhien.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BossDieuKhien : MonoBehaviour
+{
+    Animator ani;
+
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        Invoke("ChayAnimation", 2f); // gọi sau 0.5 giây
+    }
+
+    public void ChayAnimation()
+    {
+        ani.SetTrigger("BayLen");
+        ani.SetTrigger("Baytaicho");
+    }
+}
