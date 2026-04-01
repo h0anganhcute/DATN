3df5f1e41d362de82b1df6ad95280009176f1f2e
h0anganhcute <h0anganhcute66@gmail.com>
2026-04-02 01:35:17 +0700

    ok

---
+++ Assets/AAAAA/HoangAnh/Dragon/Script/DeadObj.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Unity.FPS.Game;
+
+public class CapsuleDie : MonoBehaviour
+{
+    Health mau;
+    Animator ani;
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        mau = GetComponent<Health>();
+
+        if (mau != null)
+        {
+            mau.OnDie += KhiChet;
+        }
+    }
+
+    void KhiChet()
+    {
+        Debug.Log("Capsule chết rồi");
+        ChayAnimationChet();
+        Destroy(gameObject, 5f); // delay 2 giây
+    }
+    void ChayAnimationChet()
+    {
+        ani.SetTrigger("DapXuong");
+        ani.SetTrigger("Die");
+    }
+}
---
+++ Assets/AAAAA/HoangAnh/Enemy/DieEnemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Unity.FPS.Game;
+
+public class DieEnemy : MonoBehaviour
+{
+    Health mau;
+    Animator ani;
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        mau = GetComponent<Health>();
+
+        if (mau != null)
+        {
+            mau.OnDie += KhiChet;
+        }
+    }
+    void KhiChet()
+    {
+        Debug.Log("Capsule chết rồi");
+        ani.SetTrigger("Die");
+        Destroy(gameObject, 5f); // delay 2 giây
+    }
+
+}
