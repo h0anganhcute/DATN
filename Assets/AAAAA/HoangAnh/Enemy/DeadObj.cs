772484090085c7e519e2fe959ac1411e9047d895
h0anganhcute <h0anganhcute66@gmail.com>
2026-04-01 16:24:15 +0700

    DaThanhCongHiHi

---
+++ Assets/AAAAA/HoangAnh/Enemy/DeadObj.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Unity.FPS.Game;
+
+public class CapsuleDie : MonoBehaviour
+{
+    Health mau;
+
+    void Start()
+    {
+        mau = GetComponent<Health>();
+
+        if (mau != null)
+        {
+            mau.OnDie += KhiChet;
+        }
+    }
+
+    void KhiChet()
+    {
+        Debug.Log("Capsule chết rồi");
+
+        Destroy(gameObject, 2f); // delay 2 giây
+    }
+}
