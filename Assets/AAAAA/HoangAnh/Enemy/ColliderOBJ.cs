e87c4c119c9214adc1c8c10845e67402713cf8d9
h0anganhcute <h0anganhcute66@gmail.com>
2026-04-03 14:40:22 +0700

    ok

---
+++ Assets/AAAAA/HoangAnh/Enemy/ColliderOBJ.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ColliderOBJ : MonoBehaviour
+{
+    void Start()
+    {
+        // Lấy toàn bộ Collider trong object cha + con
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+
+        // Cho tất cả collider ignore lẫn nhau
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            for (int j = i + 1; j < colliders.Length; j++)
+            {
+                Physics.IgnoreCollision(colliders[i], colliders[j], true);
+            }
+        }
+    }
+}
