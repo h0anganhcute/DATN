349efa8795db8157a20c2cabff37ad2e496aee9a
h0anganhcute <h0anganhcute66@gmail.com>
2026-04-05 03:10:46 +0700

    ok

--- Assets/AAAAA/HoangAnh/Enemy/DieEnemy.cs
+++ Assets/AAAAA/HoangAnh/Enemy/DieEnemy.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using Unity.FPS.Game;
+using UnityEngine.AI;
 
 public class DieEnemy : MonoBehaviour
 {
     Health mau;
     Animator ani;
+    [SerializeField] MonoBehaviour Run;
     void Start()
     {
         ani = GetComponent<Animator>();
@@ -17,9 +19,16 @@ public class DieEnemy : MonoBehaviour
     }
     void KhiChet()
     {
+        // Tắt não rượt đuổi
+        if (Run != null) Run.enabled = false;
+
+        // Tắt bộ di chuyển
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        if (agent != null) agent.enabled = false; // Tắt thẳng mặt, 1 dòng gọn ơ!
+
         Debug.Log("Capsule chết rồi");
         ani.SetTrigger("Die");
-        Destroy(gameObject, 5f); // delay 2 giây
+        Destroy(gameObject, 5f);
     }
 
 }
