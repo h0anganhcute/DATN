febb84b4e3bfcbce3c7307f36ce82fe2f914aedc
h0anganhcute <h0anganhcute66@gmail.com>
2026-06-05 00:34:46 +0700

    Update Skill 2 Boss

--- Assets/AAAAA/HoangAnh/Dragon/Script/DashSkill.cs
+++ Assets/AAAAA/HoangAnh/Dragon/Script/DashSkill.cs
@@ -1,21 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.AI; // Gọi thư viện AI để dùng NavMesh
 
-public class RunEnemy : MonoBehaviour
+public class DashSkill : MonoBehaviour
 {
-    Animator ani;
     [Header("Mục tiêu")]
     private Transform nguoiChoi;           // Vị trí của người chơi mà quái sẽ đuổi theo
 
     [Header("Cài đặt Di chuyển")]
-    public float tocDo = 3.5f;            // Tốc độ di chuyển của kẻ địch
+    public float tocDo = 10f;            // Tốc độ di chuyển của kẻ địch
 
     // Biến này đóng vai trò là "bộ não" giúp kẻ địch tự tìm đường và né vật cản
     private NavMeshAgent aiDiChuyen;
 
     void Start()
     {
-        ani = GetComponent<Animator>();
         // Lấy thành phần NavMeshAgent đã gắn trên Kẻ địch
         aiDiChuyen = GetComponent<NavMeshAgent>();
 
@@ -25,7 +23,7 @@ public class RunEnemy : MonoBehaviour
         // Nếu bạn quên chưa kéo Player vào ô Nguoi Choi, code sẽ tự tìm vật thể có tag "Player"
         if (nguoiChoi == null)
         {
-            GameObject timNguoiChoi = GameObject.FindGameObjectWithTag("Player");
+            GameObject timNguoiChoi = GameObject.FindGameObjectWithTag("Skill");
             if (timNguoiChoi != null)
             {
                 nguoiChoi = timNguoiChoi.transform;
@@ -37,8 +35,7 @@ public class RunEnemy : MonoBehaviour
     {
         // Kiểm tra xem đã có mục tiêu (nguoiChoi) chưa và Kẻ địch có đang đứng trên vùng di chuyển (NavMesh) không
         if (nguoiChoi != null && aiDiChuyen.isOnNavMesh)
-        {
-            ani.SetTrigger("Run");
+        {        
             // Ra lệnh cho bộ não AI liên tục đi đến vị trí hiện tại của Người chơi.
             // NavMeshAgent sẽ TỰ ĐỘNG tính toán đường đi để né các vật cản trên bản đồ.
             aiDiChuyen.SetDestination(nguoiChoi.position);
--- Assets/AAAAA/HoangAnh/Dragon/Script/Skill.cs
+++ Assets/AAAAA/HoangAnh/Dragon/Script/Skill.cs
@@ -1,13 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Skill : MonoBehaviour
 {
     private Animator ani;
+    private DashSkill dashSkill;
+    private RunEnemy runEnemy;
+
     void Start()
     {
-        ani = GetComponent<Animator>();       
+        ani = GetComponent<Animator>();
+        dashSkill = GetComponent<DashSkill>();
+
+        // Bắt buộc phải có dòng này để tìm thấy Script RunEnemy
+        runEnemy = GetComponent<RunEnemy>();
+    }
+
+    void Update()
+    {
+        // KIỂM TRA LIÊN TỤC: Nếu script RunEnemy đang bật thì bắt buộc tắt DashSkill
+        if (runEnemy != null && dashSkill != null)
+        {
+            if (runEnemy.enabled == true)
+            {
+                dashSkill.enabled = false;
+            }
+        }
     }
 
     //Skill 1: Flame Attack
@@ -15,10 +35,13 @@ public class Skill : MonoBehaviour
     {
         ani.SetTrigger("FlameAttack");
     }
+
     //Skill 2: Ice Attack
     public void Skill2()
     {
         ani.SetTrigger("TakeOff");
         ani.SetTrigger("FlyGlide");
+        ani.SetTrigger("Land");
+        dashSkill.enabled = true;
     }
 }
