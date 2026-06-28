9288bae7e1530035987157499428c3b5419af646
h0anganhcute <h0anganhcute66@gmail.com>
2026-06-28 22:56:54 +0700

    oke

--- Assets/AAAAA/HoangAnh/Player/Script/Player2Controller.cs
+++ Assets/AAAAA/HoangAnh/Player/Script/Player2Controller.cs
@@ -1,18 +1,28 @@
+using StarterAssets;
+using System.Collections;
 using UnityEngine;
 
 public class Player2Controller : MonoBehaviour
 {
     private Animator ani;
-    
+    private ThirdPersonController playerController;
     void Start()
     {
         ani = GetComponent<Animator>();
-        ani.SetTrigger("Sit");
+        
+        playerController = GetComponent<ThirdPersonController>();
+        StartCoroutine(SitDown());
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+    private IEnumerator SitDown()
+    {
+        yield return new WaitForSeconds(1f);
+        ani.SetTrigger("Sit");
+        yield return new WaitForSeconds(1f);
+        playerController.enabled = true;
     }
 }
