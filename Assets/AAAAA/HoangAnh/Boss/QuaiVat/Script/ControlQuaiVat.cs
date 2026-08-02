327bbdcdcf72a8b667b53cead2aef605720d9a3c
h0anganhcute <h0anganhcute66@gmail.com>
2026-08-02 21:31:27 +0700

    ok

---
+++ Assets/AAAAA/HoangAnh/Boss/QuaiVat/Script/ControlQuaiVat.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ControlQuaiVat : MonoBehaviour
+{
+    StartBoss startBoss;
+    TargetBoss1 targetBoss1;
+    Animator ani;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        startBoss = GetComponent<StartBoss>();
+        targetBoss1 = GetComponent<TargetBoss1>();
+        ani.SetTrigger("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void BatDau()
+    {
+              startBoss.enabled = true;
+    }
+    public void Dung()
+    {
+        startBoss.enabled = false;
+    }
+    public void BatDauTarget()
+    {
+        targetBoss1.enabled = true;
+    }
+}
---
+++ Assets/AAAAA/HoangAnh/Boss/ScriptChung/StartBoss.cs
@@ -0,0 +1,28 @@
+using GLTFast.Schema;
+using UnityEngine;
+
+public class StartBoss : MonoBehaviour
+{
+    Transform startBoss;
+    public float speed = 30f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        GameObject startBossObj = GameObject.FindGameObjectWithTag("StartBoss");
+        if (startBossObj != null)
+        {
+            startBoss = startBossObj.transform;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, startBoss.position, speed * Time.deltaTime);
+
+    }
+    
+}
