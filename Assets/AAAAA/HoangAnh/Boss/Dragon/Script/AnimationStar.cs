5686ac6fca0d2f5dd05baf1045cd1b70a1dd868f
h0anganhcute <h0anganhcute66@gmail.com>
2026-08-03 09:44:48 +0700

    ok

--- Assets/AAAAA/HoangAnh/Boss/Dragon/Script/AnimationStar.cs
+++ Assets/AAAAA/HoangAnh/Boss/Dragon/Script/AnimationStar.cs
@@ -3,16 +3,31 @@ using UnityEngine;
 public class AnimationStar : MonoBehaviour
 {
     Animator ani;
+    public GameObject cameraBoss;
+    StartBoss startBoss;
     void Start()
     {
         ani = GetComponent<Animator>();
         ani.SetTrigger("Start");
         ani.SetTrigger("Stop");
+        startBoss = GetComponent<StartBoss>();
     }
 
 
     void Update()
     {
         
+    }
+    public void tatCamera()
+    {
+               cameraBoss.SetActive(false);
+    }
+    public void batDau()
+    {
+        startBoss.enabled = true;
+    }
+    public void dung()
+    {
+        startBoss.enabled = false;
     }
 }
