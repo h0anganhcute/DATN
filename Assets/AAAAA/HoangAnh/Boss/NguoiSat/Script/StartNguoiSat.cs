c4fabf0b76f6664dbc97f7f358b3bc2bbaa328bf
h0anganhcute <h0anganhcute66@gmail.com>
2026-08-11 02:59:45 +0700

    UpdateNguoiSat

--- Assets/AAAAA/HoangAnh/Boss/NguoiSat/Script/StartNguoiSat.cs
+++ Assets/AAAAA/HoangAnh/Boss/NguoiSat/Script/StartNguoiSat.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class StartNguoiSat : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Animator ani;
+    public GameObject caMera;
     void Start()
     {
-        
+        ani = GetComponent<Animator>();
+        ani.SetTrigger("Start");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void TatCamera()
+    {
+        caMera.SetActive(false);
     }
 }
