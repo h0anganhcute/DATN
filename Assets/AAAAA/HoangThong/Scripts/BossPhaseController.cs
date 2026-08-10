05901f9836da23e14fca7b1277ec5e0a6e857c14
Lê Hoàng Thông <thongktpop112@gmail.com>
2026-08-10 20:08:21 +0700

    23

---
+++ Assets/AAAAA/HoangThong/Scripts/BossPhaseController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BossPhaseController : MonoBehaviour
+{
+    [Header("Skybox")]
+    [SerializeField] private Material skyboxPhase1;
+    [SerializeField] private Material skyboxPhase2;
+
+    private void Start()
+    {
+        // Skybox ban đầu
+        RenderSettings.skybox = skyboxPhase1;
+        DynamicGI.UpdateEnvironment();
+    }
+
+    public void ChangeToPhase2()
+    {
+        // Đổi Skybox ngay lập tức
+        RenderSettings.skybox = skyboxPhase2;
+        DynamicGI.UpdateEnvironment();
+    }
+}
