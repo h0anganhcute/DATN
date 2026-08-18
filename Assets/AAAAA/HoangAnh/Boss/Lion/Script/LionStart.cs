0ab6e0cab3c74dc28117c9279d066979bc24f839
h0anganhcute <h0anganhcute66@gmail.com>
2026-08-18 18:10:23 +0700

    ok

--- Assets/AAAAA/HoangAnh/Boss/Lion/Script/LionStart.cs
+++ Assets/AAAAA/HoangAnh/Boss/Lion/Script/LionStart.cs
@@ -3,16 +3,33 @@ using UnityEngine;
 public class LionStart : MonoBehaviour
 {
     Animator ani;
+    public GameObject Trum;
+    public GameObject caMera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ani = GetComponent<Animator>();
         ani.SetTrigger("Start");
+        ani.SetTrigger("Start1");
+        ani.SetTrigger("Start2");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void CloneTrum()
+    {
+        Trum.SetActive(true);
+    }
+    public void ActiveCamera()
+    {
+        caMera.SetActive(true);
+    }
+    public void DeactiveCamera()
+    {
+        caMera.SetActive(false);
     }
 }
